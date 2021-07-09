cdeec41561878cbdbe01350caf7550024d160799
Origami_SL <shenli@buaa.edu.cn>
2021-07-09 09:07:16 +0800

    Add files via upload

---
+++ order.sh
@@ -0,0 +1,2 @@
+python -u main_informer.py --model informer --data ETTm1 --features M --seq_len 384 --label_len 384 --pred_len 48 --e_layers 3 --d_layers 2 --attn prob --des 'Exp' --CSP --dilated --passthrough --itr 10
+
